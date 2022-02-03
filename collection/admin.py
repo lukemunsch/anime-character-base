13006626bc24d9e8a941ea651ff196627b5ed306
lukemunsch <lpmunsch@gmail.com>
2022-02-03 13:20:29 +0000

    set up summernote for character bio field

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Series, Character, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Character)
+class CharacterAdmin(SummernoteModelAdmin):
+
+    summernote_field = ['bio']
+
+admin.site.register(Series)
+
+
+admin.site.register(Comment)
--- lukeanimebase/settings.py
+++ lukeanimebase/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     # 'allauth',
     # 'allauth.account',
     # 'allauth.socialaccount',
-    # 'django_summernote',
+    'django_summernote',
     'collection',
 ]
 
