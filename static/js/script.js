532cc30a94c9cd4e2b85e60eb09918623a62d129
lukemunsch <lpmunsch@gmail.com>
2022-02-03 22:37:00 +0000

    change content for js file

--- static/js/script.js
+++ static/js/script.js
@@ -2,10 +2,3 @@ $(".nav .nav-link").on("click", function(){
     $(".nav").find(".active").removeClass("active");
     $(this).addClass("active");
  });
-
- 
-let setTimeout(function () {
-    let messages = document.getElementById('msg');
-    let alert = new bootstrap.Alert(messages);
-    alert.close();
-}, 3000);
