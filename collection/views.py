84751f0fd11ce6fe544096a52a86520a7ddd49ce
lukemunsch <lpmunsch@gmail.com>
2022-02-03 15:03:56 +0000

    complete view for Character and create base.html file

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+"""Set up the view for our models"""
+from django.shortcuts import render, request
+from django.views import generic
+from .models import Character, Series, Comment
 
-# Create your views here.
+
+def Index(request):
+    return render(request, 'index.html')
+
+
+class CharacterCards(generic.ListView):
+    """this is the display of our Character model"""
+    model = Character
+    queryset = Character.objects.filter(status=1).order_by('-created_on')
+    template_name = 'characters.html'
+    paginate_by = 10
