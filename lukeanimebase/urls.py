8e2c1d448608a99d3c268e3934c54867982caafe
lukemunsch <lpmunsch@gmail.com>
2022-02-04 22:36:17 +0000

    copying required authorisation page files for editing

--- lukeanimebase/urls.py
+++ lukeanimebase/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('collection.urls'), name='collection_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
