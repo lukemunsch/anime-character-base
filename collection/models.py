33711217105d89d2d6ff0b3e27600d07b95373f9
lukemunsch <lpmunsch@gmail.com>
2022-02-10 15:43:01 +0000

    fix code issue with not generating a slug

--- collection/models.py
+++ collection/models.py
@@ -1,6 +1,7 @@
 """Set up our imports"""
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 from cloudinary.models import CloudinaryField
 
 
@@ -45,6 +46,11 @@ class Character(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f'{self.series_name}-{self.name}')
+        return super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
