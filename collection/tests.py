590da61f23d446391b796062e3b9e199f6445673
lukemunsch <lpmunsch@gmail.com>
2022-02-22 21:40:23 +0000

    fix issue with html validation checks

--- collection/tests.py
+++ collection/tests.py
@@ -1,8 +1,35 @@
 from django.test import TestCase
+from .models import (
+    Character,
+    Series,
+    Suggestion,
+    Comment
+)
+from .forms import (
+    CreateCharForm,
+    CreateSugForm,
+    AddSerForm,
+    ComForm
+)
+from .views import (
+    create_char,
+    create_series,
+    create_sug,
+    edit_char,
+    edit_series,
+    delete_char,
+    delete_comm,
+    delete_series,
+    delete_sug,
+    CharacterList,
+    CharDetail,
+    SeriesList,
+    SuggestionList,
+)
 
 # Create your tests here.
-class Test(TestCase):
+class TestModels(TestCase):
     """generic test"""
-    def test_this_works(self):
-        """"""
-        self.assertEqual(1, 0)
+    def test_character_model(self):
+        """set up tests for character model"""
+        
